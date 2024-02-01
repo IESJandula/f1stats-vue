2f88bbec2cf26fdb09cba14f68a43724d981c35d
franklin3499@gmail.com <145580354+francis65000@users.noreply.github.com>
2024-02-01 12:37:01 +0100

    Moviendo proyecto a rama

---
+++ F1/F1/src/components/TarjetaCircuito.vue
@@ -0,0 +1,11 @@
+<script setup>
+/*Esta clase debe recibir como parametro del padre un valor de tipo objeto, con los datos de un circuito*/
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped>
+
+</style>
